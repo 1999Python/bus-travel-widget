b1bdde6bbe1cf40759cc7a7c747c14505cc7b409
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-05-31 15:56:06 +0200

    final attempt

--- dom.js
+++ dom.js
@@ -29,49 +29,77 @@ const pricePerTripReturn = document.querySelector(".pricePerTripReturn");
 
 const busTravelWidget = BusTravelWidget();//factory function 
 ///////////////////////////////////////////////////////////////////////////////
+// im trying to retrieve my factory functions to get the totals for the following :
+//The total number of trips she can take
+//The cost price per trip (single)
+//The number of return trips she can make
+//The cost per return trip
+function mainfunctionTotals(){
+    pricePerTripReturn.innerHTML = busTravelWidget.pricePerTripReturn();
+    numberReturnTrips.innerHTML = busTravelWidget.numberReturnTrips();
+    pricePerTripSingle.innerHTML = busTravelWidget.pricePerTripSingle();
+    numberSingleTrips.innerHTML = busTravelWidget.numberSingleTrips();
+}
+calculateBtn.addEventListener("click", mainfunctionTotals);//event listener for the calculate button
 
+////////////////////////////////////////////////////////////////////////////////
 function TravelTimes() {
     //tryna check if she traveled peak or off peek times (which one was selected) 
-        var checkedRadioBtn = document.querySelector("input[name='travelTimes']:checked");
-    
-        if (checkedRadioBtn) {
-    
-// travelTimes will be 'Peak' or 'off-Peak' calculation was done on the factory function which I am about to link.       
-if (checkedRadioBtn.value === "Peak") {
-                busTravelWidget.peakValue();
-//what im tryna say is that if the checkedradiobtn gets the value of "call" /"sms" 
-//then we should call the baby function(Closures are baby functions) peakValue() and offPeakValue() to determine the value
-            }
-            else if (checkedRadioBtn.value === "Off-Peak") {
-                busTravelWidget.offPeakValue();
-            }
+    var checkedRadioBtn = document.querySelector("input[name='travelTimes']:checked");
+
+    if (checkedRadioBtn) {
+
+        // travelTimes will be 'Peak' or 'off-Peak' calculation was done on the factory function which I am about to link.       
+        if (checkedRadioBtn.value === "Peak") {
+            busTravelWidget.peakValue();
         }
-        
-    
-    
+        //what im tryna say is that if the checkedradiobtn gets the value of "call" /"sms" 
+        //then we should call the baby function(Closures are baby functions) peakValue() and offPeakValue() to determine the value
+
+        else if (checkedRadioBtn.value === "Off-Peak") {
+            busTravelWidget.offPeakValue();
+        }
     }
-    itemTypeRadio.addEventListener("click", TravelTimes);
+
+}
+itemTypeRadio.addEventListener("click", TravelTimes);//event listener for the radio buttons
+
+///////////////////////////////////////////////////////////////////////////////
+function returnTripFunc() {
+    var checkedBoxBtn = document.querySelector("input[name='returnTripCheckbox']:checked");
+//What I am trying to say is that if the checked box is seleceted it should return a value of return trips
+
+    if (checkedBoxBtn) {
+        if (checkedBoxBtn.value === "Return Trip") {
+            busTravelWidget.returnTrip();
+        }
+    }
+}
+returnTripCheckbox.addEventListener("click", returnTripFunc);//event listener for the checked box
+///////////////////////////////////////////////////////////////////////////////
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function dropbtnLocation() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  window.onclick = function(event) {
+}
+window.onclick = function (event) {
 
     if (!event.target.matches('.dropbtn')) {
 
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-    
-      for (var i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        
-        if (openDropdown.classList.contains('show')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
 
-          openDropdown.classList.remove('show');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+
+            if (openDropdown.classList.contains('show')) {
+
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-    dropdowns = busTravelWidget.startLocation();
-  }
-  dropdowns.addEventListener("click",  dropbtnLocation);
+    dropdowns.innerHTML = busTravelWidget.startLocation();
+}
+dropdowns.addEventListener("click", dropbtnLocation);//event listener for the drop down menu for the location
+// that needs to be selected I also included the factory function which will then add a certain amount of points
+// depending on which location was selected. 
--- script.js
+++ script.js
@@ -1,74 +1,87 @@
 function BusTravelWidget() {
-   var locationAmount = 0;
-   var singleTrip = 0;
+//variables created to hold the totals and calculations 
+    var locationAmount = 0;
+    var singleTrip = 0;
+    var totalTrips = 0;
+    var singleReturn = 0;
+    var totalReturn = 0;
+  
 
-    var billTotal = 0;
-
-    let location = {"Dunoon": 25, "Khayelitsha": 40, "Mitchells Plain": 30}
+    let location = { "Dunoon": 25, "Khayelitsha": 40, "Mitchells Plain": 30 }
 
     function pointsFunction() {
-
+//trying to get the amount of points for the location chosen (for in looping)
         let dropdowns = "";
 
         for (let key in location) {
             dropdowns += location[key];
-          }
+        }
 
     }
 
     function returnTrip() {
-        return startLocation;
+        if (locationAmount === "Dunoon") {
+            singleTrip = points / 50;
+        }
+        if (locationAmount === "Mitchells Plain") {
+            singleTrip = points / 60;
+        }
+        if (locationAmount === "Khayelitsha") {
+            singleTrip = points / 80;
+        }
     }
 
-    function peakValue() {
-        offPeakValue() * 25 / 100;
+    function peakValue() { ///On peak hours you have to pay 25% more than the normal price
+       locationAmount * 25 / 100;
 
     }
 
-    function offPeakValue() {
+    function offPeakValue() {// normal amount without the peak price 
 
-      return pricePerTripSingle();
+        return locationAmount;
 
     }
 
     function startLocation(value) {
-    
-       locationAmount = value
-       return locationAmount
+
+        locationAmount = value
+        return locationAmount
 
     }
 
- 
+
     function numberSingleTrips() {
-      
-            if (locationAmount === "Dunoon") {
-                singleTrip = points /25;
-            }
-            if (locationAmount === "Mitchells Plain") {
-                singleTrip = points /30;
-            }
-            if (locationAmount === "Khayelitsha") {
-                singleTrip = points /40;
-            }
-     }
 
-    function pricePerTripSingle() {
-
-        if (location.Dunoon === 25) {
-            billTotal += 25;
+        if (locationAmount === "Dunoon") {
+            singleTrip = points / 25;
         }
-        if (location.Khayelitsha === 40) {
-        billTotal += 40;
+        if (locationAmount === "Mitchells Plain") {
+            singleTrip = points / 30;
         }
-        if (location.MitchellsPlain === 30) {
-            billTotal += 30;
+        if (locationAmount === "Khayelitsha") {
+            singleTrip = points / 40;
         }
     }
 
-    function numberReturnTrips() { }
+    function pricePerTripSingle(amount) {
+
+        if (amount -= 25) {
+            "Dunoon";
+        }
+        if (amount -= 40) {
+            "Khayelitsha";
+        }
+        if (amount -= 30) {
+            "Mitchells Plain";
+        }
+    }
+
+    function numberReturnTrips() { 
+       return totalReturn
+    }
 
     function pricePerTripReturn() {
-
+        return singleReturn
     }
 
 
