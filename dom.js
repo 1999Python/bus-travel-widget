fd7c0725f3e5520a0926162a0442c85f04a582fb
Raadiyah Arend <raadiyaharend6@gmail.com>
2023-05-31 11:52:02 +0200

    back up 2

--- dom.js
+++ dom.js
@@ -1,24 +1,64 @@
 //Selecting all the elements and buttons
-const Points = document.querySelector(".Points");
 
-const enterButton = document.querySelector(".enter");
+const Points = document.querySelector(".Points");
+//An amount of points
+
+const startLocation = document.querySelector(".dropbtn");
+//This is a separate function on its own this drop down button will select which loacation she starts at.
 
 const itemTypeRadio = document.querySelector(".itemTypeRadio");
+//radio buttons select either Peak or off peak
 
 const returnTripCheckbox = document.querySelector(".returnTripCheckbox");
+//checkbox for return trips oly selected if she had a return trip
 
 const calculateBtn = document.querySelector(".calculateBtn");
+//calculate button
 
 const numberSingleTrips = document.querySelector(".numberSingleTrips");
+//The total number of trips she can take
 
 const pricePerTripSingle = document.querySelector(".pricePerTripSingle");
+//The cost price per trip (single)
 
 const numberReturnTrips = document.querySelector(".numberReturnTrips");
+//The number of return trips she can make
 
 const pricePerTripReturn = document.querySelector(".pricePerTripReturn");
+//The cost per return trip
 
 const busTravelWidget = BusTravelWidget();//factory function 
+///////////////////////////////////////////////////////////////////////////////
 
+function TravelTimes() {
+    //tryna check if she traveled peak or off peek times (which one was selected) 
+        var checkedRadioBtn = document.querySelector("input[name='travelTimes']:checked");
+    
+        if (checkedRadioBtn) {
+    
+            // billItemType will be 'call' or 'sms'
+            if (checkedRadioBtn.value === "Peak") {
+                busTravelWidget.sendsms();
+                //what im tryna say is that if the checkedradiobtn gets the value of "call" /"sms" 
+                //then we should call the baby function makeCall and the same for sms 
+            }
+            else if (checkedRadioBtn.value === "Off-Peak") {
+                busTravelWidget.makeCall();
+    
+            }
+    
+        }
+    
+        // callTotalSettings.innerHTML = billSettings.getTotalcallCost().toFixed(2);
+    
+        // smsTotalSettings.innerHTML = billSettings.getTotalsmsCost().toFixed(2);
+    
+        // totalSettings.innerHTML = billSettings.getTotalCost().toFixed(2);
+    
+    
+    }
+    
+    itemTypeRadio.addEventListener("click", TravelTimes);
 
 
 
--- script.js
+++ script.js
@@ -1,14 +1,37 @@
-BusTravelWidget(
+function BusTravelWidget() {
+
+    function points() { }
+
+    function returnTrip() { }
+
+    function peakValue(){
+    }
+
+    function offPeakValue(){
+    }
+    function startLocation(){
+    }
+
+    function numberSingleTrips(){}
+
+    function pricePerTripSingle(){}
+
+    function numberReturnTrips(){}
+
+    function pricePerTripReturn(){}
 
 
+    return {
+        points,
+        returnTrip,
+        peakValue,
+        offPeakValue,
+        numberSingleTrips,
+        pricePerTripSingle,
+        numberReturnTrips,
+        pricePerTripReturn,
+        startLocation
+    }
 
 
-
-
-
-
-
-
-
-
-)
+}
